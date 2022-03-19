28f81654a416719e2b14c8367c893ad4228241f8
Howler Nouvelle <hilmy@wahidin.net>
2022-03-19 16:45:36 +0700

    implementasi-generic-class

--- app/src/main/java/com/barney/kpljurnalmod4/MainActivity.java
+++ app/src/main/java/com/barney/kpljurnalmod4/MainActivity.java
@@ -3,6 +3,7 @@ package com.barney.kpljurnalmod4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView darto  = findViewById(R.id.mio);
+
+        SimpleDataBase<Double> perjuangan = new SimpleDataBase(darto);
+
+        perjuangan.AddNewData(12.0);
+        perjuangan.AddNewData(34.0);
+        perjuangan.AddNewData(56.0);
+
+        perjuangan.PrintAllData();
+
     }
 }
